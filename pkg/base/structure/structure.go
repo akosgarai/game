470ff837127e19212e1e3a03fde852e136e143ca
akosgarai <szlem01@gmail.com>
2018-05-20 18:08:15 +0200

    Implement basic structure functionalities

--- pkg/base/structure/structure.go
+++ pkg/base/structure/structure.go
@@ -1,1 +1,35 @@
 package structure
+
+type Resource interface {
+    Harvest(int) error
+    Produce(int)
+    GetName() string
+    GetAmount() int
+}
+
+type Structure struct {
+    name string
+    NeededResource Resource
+    ProducedResource Resource
+}
+
+func Empty() *Structure {
+    return &Structure{}
+}
+
+func New (name string, needed Resource, produced Resource) *Structure {
+    return &Structure{
+        name: name,
+        NeededResource: needed,
+        ProducedResource: produced,
+    }
+}
+func (s *Structure) GetName() string {
+    return s.name
+}
+func (s *Structure) GetNeededResource() Resource {
+    return s.NeededResource
+}
+func (s *Structure) GetProducedResource() Resource {
+    return s.ProducedResource
+}
