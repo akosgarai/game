92100cd8e24b5b63081be8885edcf987c7ba2855
akosgarai <szlem01@gmail.com>
2018-05-20 22:02:59 +0200

    Planet - New function

--- pkg/base/planet/planet.go
+++ pkg/base/planet/planet.go
@@ -1,7 +1,20 @@
 package planet
 
 import (
-    "errors"
-    "github.com/akosgarai/game/pkg/base/resource"
     "github.com/akosgarai/game/pkg/base/area"
 )
+
+type Planet struct {
+    Name string
+    Area []area.Area
+}
+
+func New(name string, size int) *Planet{
+    var p = Planet{
+        Name: name,
+    }
+    for i := 0; i < size; i++ {
+        p.Area = append(p.Area, *area.New())
+    }
+    return &p
+}
--- pkg/base/planet/planet_test.go
+++ pkg/base/planet/planet_test.go
@@ -7,3 +7,13 @@ import(
 func TestEmpty(t *testing.T) {
     t.Skip("Empty")
 }
+
+func TestNew(t *testing.T) {
+    p := New("TestName", 3)
+    if p.Name != "TestNew" {
+        t.Error("Invalid name for plaet")
+    }
+    if len(p.Area) != 3 {
+        t.Error("Area length invalid")
+    }
+}
