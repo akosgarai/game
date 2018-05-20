f77322f0d69eaab82a93ed2f69f6f953911f6aa0
akosgarai <szlem01@gmail.com>
2018-05-20 14:33:07 +0200

    New & Harvest functions

--- pkg/base/resource/resource.go
+++ pkg/base/resource/resource.go
@@ -1,1 +1,27 @@
 package resource
+
+import(
+    "errors"
+    "strconv"
+)
+
+type Resource struct {
+    Name string
+    Amount int
+}
+
+func New(name string, amount int) *Resource {
+    return &Resource{
+        Name: name,
+        Amount: amount,
+    }
+}
+
+func (r *Resource) Harvest(amount int) error {
+    if r.Amount < amount {
+        message := "Insufficient. Need: " + strconv.Itoa(amount) + ", Amount: " + strconv.Itoa(r.Amount)
+        return errors.New(message)
+    }
+    r.Amount = r.Amount - amount
+    return nil
+}
--- pkg/base/resource/resource_test.go
+++ pkg/base/resource/resource_test.go
@@ -4,6 +4,50 @@ import(
     "testing"
 )
 
-func TestBase(t *testing.T) {
-    t.Skip("Unimplemented")
+func TestNew(t *testing.T) {
+
+    var testData = []struct {
+        Name string
+        Amount int
+    } {
+        {"test1", 10},
+        {"test2", 20},
+    }
+
+    for _, tt := range testData {
+        resource := New(tt.Name, tt.Amount)
+        if resource.Name != tt.Name {
+            t.Errorf("Name suppose to be same. %s instead of %s", resource.Name, tt.Name)
+        }
+        if resource.Amount != tt.Amount {
+            t.Errorf("Amount suppose to be same. %d instead of %d", resource.Amount, tt.Amount)
+        }
+    }
+}
+
+func TestHarvest(t *testing.T) {
+    r := Resource{
+        Name : "goo",
+        Amount: 1000,
+    }
+    r.Harvest(10)
+    if r.Amount != 990 {
+        t.Error("It suppose to be 990")
+    }
+    r.Harvest(10)
+    if r.Amount != 980 {
+        t.Error("It suppose to be 980")
+    }
+    r.Harvest(10)
+    if r.Amount != 970 {
+        t.Error("It suppose to be 970")
+    }
+    err := r.Harvest(1000)
+    if err == nil {
+        t.Error("It suppose to be error")
+    }
+    expected_error := "Insufficient. Need: 1000, Amount: 970"
+    if err.Error() != expected_error {
+        t.Error("Unexpected error message")
+    }
 }
