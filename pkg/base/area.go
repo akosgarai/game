92d0643d59ca6d27f0b0d7331149f8761d373655
akosgarai <szlem01@gmail.com>
2018-05-20 13:22:16 +0200

    Improvement

--- pkg/base/area.go
+++ pkg/base/area.go
@@ -1,11 +1,40 @@
 package area
 
+import (
+    "errors"
+    "strconv"
+    "fmt"
+)
+
+var LOG_LEVEL = "ERROR"
+
 type Resource struct {
     Name string
+    Amount int
+}
+
+func logger(message string) {
+    if LOG_LEVEL == "DEBUG" {
+        fmt.Print(message)
+    }
+}
+
+func (r *Resource) Harvest(amount int) error {
+    message := "Harvesting from resource '" + r.Name + "' " + strconv.Itoa(amount) + " / " + strconv.Itoa(r.Amount) + "\n"
+    logger(message)
+    if r.Amount < amount {
+        return errors.New("Insufficient number of resources.")
+    }
+    r.Amount = r.Amount - amount
+    message = "Harvesting from resource '" + r.Name + "' " + strconv.Itoa(amount) + " / " + strconv.Itoa(r.Amount) + "\n"
+    logger(message)
+    return nil
 }
 
 type Structure struct {
     Name string
+    NeededResource Resource
+    ProducedResource Resource
 }
 
 type Area struct {
@@ -28,4 +57,28 @@ func (a *Area) AddResource(r Resource) error {
 func (a *Area) Build(s Structure) error {
     a.Building = s
     return nil
+}
+
+func (a *Area) Harvest () (Resource, error) {
+
+    if a.Building.Name == "" {
+        return Resource{}, nil
+    }
+    if a.Building.ProducedResource.Name == "" {
+        return Resource{}, nil
+    }
+    if a.Building.NeededResource.Name == "" {
+        return a.Building.ProducedResource, nil
+    }
+    for index, r := range a.Resources {
+        if r.Name == a.Building.NeededResource.Name {
+            if err := r.Harvest(a.Building.NeededResource.Amount); err != nil {
+                return Resource{}, err
+            }
+            a.Resources[index] = r
+            return a.Building.ProducedResource, nil
+        }
+    }
+
+    return Resource{}, errors.New("Resource missing.")
 }
--- pkg/base/area_test.go
+++ pkg/base/area_test.go
@@ -25,6 +25,7 @@ func TestAddResource(t *testing.T) {
 
     r := Resource{
         Name : "goo",
+        Amount: 1000,
     }
     area.AddResource(r)
     if len(area.Resources) != 1 {
@@ -32,6 +33,9 @@ func TestAddResource(t *testing.T) {
     }
     if area.Resources[0].Name != "goo" {
         t.Error("It suppose to be goo")
+    }
+    if area.Resources[0].Amount != 1000 {
+        t.Error("It suppose to be 1000")
     }
 }
 
@@ -43,5 +47,107 @@ func TestBuild(t *testing.T) {
     area.Build(s)
     if area.Building.Name != "Factory" {
         t.Error("It suppose to be Factory")
+    }
+    s = Structure{
+        Name: "House",
+    }
+    area.Build(s)
+    if area.Building.Name != "House" {
+        t.Error("It suppose to be House now")
+    }
+}
+func TestResourceHarvest(t *testing.T) {
+    r := Resource{
+        Name : "goo",
+        Amount: 1000,
+    }
+    r.Harvest(10)
+    if r.Amount != 990 {
+        t.Error("It suppose to be 990")
+    }
+    r.Harvest(10)
+    if r.Amount != 980 {
+        t.Error("It suppose to be 980")
+    }
+    r.Harvest(10)
+    if r.Amount != 970 {
+        t.Error("It suppose to be 970")
+    }
+    err := r.Harvest(1000)
+    if err == nil {
+        t.Error("It suppose to be error")
+    }
+}
+func TestAreaHarvest(t *testing.T) {
+    area := New()
+    resource, err := area.Harvest()
+    if err != nil {
+        t.Error("Without building we shouldn't have Error")
+    }
+    if resource.Name != "" {
+        t.Error("Without building we shouldn't have Resource")
+    }
+    building := Structure{
+        Name: "House",
+        NeededResource: Resource{},
+        ProducedResource: Resource{
+            Name: "Stuff",
+            Amount: 3,
+        },
+    }
+    area.Build(building)
+    resource, err = area.Harvest()
+    if err != nil {
+        t.Error("With this building we shouldn't have Error")
+    }
+    if resource.Name != "Stuff" {
+        t.Error("With this building we should have Stuff")
+    }
+    if resource.Amount != 3 {
+        t.Error("With this building we should have 3 Stuff")
+    }
+    building = Structure{
+        Name: "Factory",
+        NeededResource: Resource{
+            Name: "Iron",
+            Amount: 10,
+        },
+        ProducedResource: Resource{
+            Name: "Car",
+            Amount: 1,
+        },
+    }
+    area.Build(building)
+    resource, err = area.Harvest()
+    if err == nil {
+        t.Error("With this building we should have Error")
+    }
+    if err.Error() != "Resource missing." {
+        t.Error("With this building we should have different Error")
+    }
+    if resource.Name != "" {
+        t.Error("With this building we shouldn't have name")
+    }
+    new_resource := Resource{
+        Name: "Iron",
+        Amount: 1000,
+    }
+    area.AddResource(new_resource)
+    resource, err = area.Harvest()
+    if err != nil {
+        t.Error("Now, we shouldn't have Error")
+    }
+    if resource.Name != "Car" {
+        t.Error("With this building we should have Car")
+    }
+    if resource.Amount != 1 {
+        t.Error("With this building we should have 1 Car")
+    }
+    for _, r := range area.Resources {
+        if r.Name == "Iron" {
+            if r.Amount != 990 {
+                t.Errorf("This building should use 10 resource for it's output. Amount: %d", r.Amount)
+            }
+        }
     }
 }
