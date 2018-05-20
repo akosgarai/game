dd5bd4e1f563282e966ba2a68ae991c6d06ab9d9
akosgarai <szlem01@gmail.com>
2018-05-20 19:39:11 +0200

    Remove some unnecessary stuff

--- pkg/base/structure/structure.go
+++ pkg/base/structure/structure.go
@@ -6,15 +6,15 @@ import(
 
 type Structure struct {
     name string
-    NeededResource resource.Resource
-    ProducedResource resource.Resource
+    NeededResource *resource.Resource
+    ProducedResource *resource.Resource
 }
 
 func Empty() *Structure {
     return &Structure{}
 }
 
-func New (name string, needed resource.Resource, produced resource.Resource) *Structure {
+func New (name string, needed *resource.Resource, produced *resource.Resource) *Structure {
     return &Structure{
         name: name,
         NeededResource: needed,
@@ -24,9 +24,9 @@ func New (name string, needed resource.Resource, produced resource.Resource) *St
 func (s *Structure) GetName() string {
     return s.name
 }
-func (s *Structure) GetNeededResource() resource.Resource {
+func (s *Structure) GetNeededResource() *resource.Resource {
     return s.NeededResource
 }
-func (s *Structure) GetProducedResource() resource.Resource {
+func (s *Structure) GetProducedResource() *resource.Resource {
     return s.ProducedResource
 }
