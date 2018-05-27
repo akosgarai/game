d9f96dcc35f043e07158b5c8aeca8cf1dc5e9d62
akosgarai <szlem01@gmail.com>
2018-05-27 15:48:39 +0200

    connect population with planet

--- pkg/base/planet/planet.go
+++ pkg/base/planet/planet.go
@@ -2,22 +2,41 @@
 package planet
 
 import (
+	"errors"
     "github.com/akosgarai/game/pkg/base/area"
+    "github.com/akosgarai/game/pkg/base/interfaces"
 )
 
 // Planet has Name and size (represented by the number of the Areas)
+// Name - Name of the planet.
+// Area - Size of the planet - 1 area / size.
+// Population - the speacies that lives in the planet.
+// isPopulated - true if the planet is populated, Population is set and not extincted.
 type Planet struct {
     Name string
     Area []area.Area
+    Population interfaces.Resource
+    isPopulated bool
 }
 
 // New returns *Planet with the given name and size.
 func New(name string, size int) *Planet{
     var p = Planet{
         Name: name,
+        isPopulated: false,
     }
     for i := 0; i < size; i++ {
         p.Area = append(p.Area, *area.New())
     }
     return &p
+}
+
+// Populate add Population resource to the planet.
+func (p *Planet) Populate (r interfaces.Resource) error {
+    if (p.isPopulated) {
+        return errors.New("Planet populated.")
+    }
+    p.Population = r
+    p.isPopulated = true
+    return nil
 }
--- pkg/base/planet/planet_test.go
+++ pkg/base/planet/planet_test.go
@@ -2,6 +2,7 @@ package planet
 
 import(
     "testing"
+    "github.com/akosgarai/game/pkg/base/speacies"
 )
 
 func TestEmpty(t *testing.T) {
@@ -15,5 +16,18 @@ func TestNew(t *testing.T) {
     }
     if len(p.Area) != 3 {
         t.Error("Area length invalid")
+    }
+}
+
+func TestPopulate(t *testing.T) {
+    p := New("TestName", 3)
+    guys := speacies.New("test race", 1)
+    err := p.Populate(guys)
+    if err != nil {
+        t.Error("Population suppose to be successful first")
+    }
+    err = p.Populate(guys)
+    if err == nil {
+        t.Error("Population suppose to be errored next time")
     }
 }
